2eff3bbfc35ba7628825f9db597b2d7f0a71e802
harshit joshi <hj7656273@gmail.com>
2025-08-26 23:43:07 +0530

    learning

---
+++ jwttoken/app.js
@@ -0,0 +1,17 @@
+const cookieParser = require('cookie-parser');
+const express=require('express');
+const app=express();
+
+const usermodel=require('./models/user')
+const path=require('path')
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))   
+app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser()) 
+app.get('/',(req,res)=>{
+    res.render('index')
+});
+app.listen(3000,()=>{
+    console.log('http://localhost:3000');
+})
